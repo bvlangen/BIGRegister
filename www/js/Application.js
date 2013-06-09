35d83269e8acc2369040cc009b3ec02138825b67
Bert van Langen <bert.vanlangen@gmail.com>
2013-06-09 17:18:06 +0200

    Enable menubutton on device to open navbar & minimized navbar

--- www/js/Application.js
+++ www/js/Application.js
@@ -14,6 +14,7 @@ var resultView;
         searchView = new SearchView();
         resultView = new ResultView();
         appController.init();
+        document.addEventListener("menubutton", function(){$('#menu').collapse('toggle')}, false);
     };
 
     var init = function() {
