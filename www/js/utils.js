e0c1a24c65959d5e20a44432f89ea5b21b46f06c
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-06-13 15:41:19 +0200

    alert js errors

--- www/js/utils.js
+++ www/js/utils.js
@@ -1,15 +1,13 @@
 "use strict";
 
+window.onerror = function(message, file, line) {
+  alert('Error gevangen: ' + file + ':' + line + '\n' + JSON.stringify(message));
+};
+
 function isDefined(variable) {
     return variable !=null && variable !== 'undefined' && variable != ''
 
 }
-
-//function googleAnalytics(page) {
-//    if (gaPlugin !== undefined) {
-//        gaPlugin.trackPage(emptyCallback, emptyCallback, page);
-//    }
-//}
 
 function isAndroid() {
     return navigator.userAgent.toLowerCase().indexOf("android") > -1;
